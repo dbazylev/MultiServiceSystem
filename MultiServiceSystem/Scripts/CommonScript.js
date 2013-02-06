a77c866064d4e94f283eba3826b0ea5fbb2a0812
Dima Bazylev <dbazylev@gmail.com>
2013-02-07 00:28:46 +0300

    Initialize commit

---
+++ MultiServiceSystem/Scripts/CommonScript.js
@@ -0,0 +1,22 @@
+﻿function executeRandomGenerator() {
+    var a = collectValue("input_a");
+    var c = collectValue("input_c");
+    var m = collectValue("input_m");
+    var generator = new LehmerRandomGenerator(a, c, m, 0);
+    var randomValues = [];
+    var n=30;
+    for (var i = 0; i < n; i++) {
+        randomValues[i] = generator.GetRandom();
+    }
+    
+    printValues(randomValues);
+}
+
+
+function collectValue(id) {
+    return document.getElementById(id).value;
+}
+
+function printValues(array) {
+    alert('ok');
+}
---
+++ MultiServiceSystem/Scripts/LehmerRandomGenerator.js
@@ -0,0 +1,12 @@
+﻿function LehmerRandomGenerator(a, c, m, initValue) {
+    this.a = a;
+    this.c = c;
+    this.m = m;
+    this.previousValue = initValue;
+    this.GetRandom=function()
+    {
+        this.previousValue = (this.a * this.previousValue + this.c) % this.m;
+        return this.previousValue / this.m;
+    }
+}
+
