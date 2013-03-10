5be2de462aeedc9bdf8f02eeea53590e3023ca69
dbazylev <dbazylev@gmail.com>
2013-03-11 00:22:06 +0300

    Adapt data to chart

--- MultiServiceSystem/Scripts/CommonScript.js
+++ MultiServiceSystem/Scripts/CommonScript.js
@@ -3,15 +3,15 @@
     var c = collectValue("input_c");
     var m = collectValue("input_m");
     var x = collectValue("input_x0");
+    var n = collectValue("input_n");
     var generator = new LehmerRandomGenerator(a, c, m, x);
     var randomValues = [];
-    var n=30;
     for (var i = 0; i < n; i++) {
         randomValues[i] = generator.GetRandom();
     }
     
     printValues(randomValues);
-    drowChart();
+    drowChart(randomValues);
 
 }
 
@@ -35,19 +35,44 @@ function printValues(array) {
     printValue("out_verification", verification);
 }
 
-function drowChart() {
+function drowChart(values) {
     var canvas = document.getElementById("canvas");
-    var arr = [];
-    arr[0] = new Bar(2, 0);
-    arr[1] = new Bar(1, 0.1);
-    arr[2] = new Bar(0, 0.2);
-    arr[3] = new Bar(4, 0.3);
-    arr[4] = new Bar(8, 0.4);
-    arr[5] = new Bar(2, 0.5);
-    arr[6] = new Bar(5, 0.6);
-    arr[7] = new Bar(5, 0.7);
-    arr[8] = new Bar(5, 0.8);
-    arr[9] = new Bar(5, 0.9);
-    buildChart(canvas, arr);
+    var bars = [];
+    bars[0] = new Bar(0, 0);
+    bars[1] = new Bar(0, 0.1);
+    bars[2] = new Bar(0, 0.2);
+    bars[3] = new Bar(0, 0.3);
+    bars[4] = new Bar(0, 0.4);
+    bars[5] = new Bar(0, 0.5);
+    bars[6] = new Bar(0, 0.6);
+    bars[7] = new Bar(0, 0.7);
+    bars[8] = new Bar(0, 0.8);
+    bars[9] = new Bar(0, 0.9);
+    
+    buildChart(canvas, fillBars(bars, values));
 }
 
+function fillBars(bars, values) {
+    for (var i = 0; i < values.length; i++) {
+        var j = 0;
+        var categoryIndex=-1;
+        while (j < bars.length-1) {
+            if (bars[j].label < values[i] && bars[j + 1].label > values[i]) {
+                categoryIndex = j;
+                break;
+            }
+            j++;
+        }
+        
+        if(categoryIndex==-1) {
+            categoryIndex = bars.length-1;
+        }
+
+        bars[categoryIndex].value++;
+    }
+
+    for (var i = 0; i<bars.length; i++) {
+        bars[i].value = (bars[i].value / values.length) /0.025;
+    }
+    return bars;
+}
